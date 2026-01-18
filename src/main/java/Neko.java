bb10cec2a721a06c0d1f7d428e74605c9e585fa7
Teh Hock Jian <hockjianteh@gmail.com>
2026-01-18 19:31:45 +0800

    Remove old line from Neko.java

--- src/main/java/Neko.java
+++ src/main/java/Neko.java
@@ -1,10 +1,11 @@
 public class Neko {
     public static void main(String[] args) {
-        String logo = "____________________________________________________________\n"
+        String message = "____________________________________________________________\n"
                 + " Hello! I'm Neko\n"
                 + " What can I do for you?\n"
                 + "____________________________________________________________\n"
                 + " Bye. Hope to see you again soon!\n"
                 + "____________________________________________________________";
+        System.out.println(message);
     }
 }
