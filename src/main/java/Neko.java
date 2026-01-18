d6d49ef8c23da67a5ecc8c6136b68b71cc308a3c
Teh Hock Jian <hockjianteh@gmail.com>
2026-01-18 19:30:27 +0800

    Remove old line from Neko.java

--- src/main/java/Neko.java
+++ src/main/java/Neko.java
@@ -6,6 +6,5 @@ public class Neko {
                 + "____________________________________________________________\n"
                 + " Bye. Hope to see you again soon!\n"
                 + "____________________________________________________________";
-        System.out.println("Hello from\n" + logo);
     }
 }
