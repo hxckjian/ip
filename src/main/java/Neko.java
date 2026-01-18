f20326bad7cece294e27db6585428e838e7f092d
Teh Hock Jian <hockjianteh@gmail.com>
2026-01-18 20:02:07 +0800

    Add functionality of echoing commands entered by the user

--- src/main/java/Neko.java
+++ src/main/java/Neko.java
@@ -1,11 +1,25 @@
+import java.util.Scanner;
+
 public class Neko {
     public static void main(String[] args) {
         String message = "____________________________________________________________\n"
                 + " Hello! I'm Neko\n"
                 + " What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + " Bye. Hope to see you again soon!\n"
                 + "____________________________________________________________";
         System.out.println(message);
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        while (!input.equals("bye")) {
+            String echoMessage = "____________________________________________________________\n"
+                    + " "
+                    + input
+                    + "\n____________________________________________________________";
+            System.out.println(echoMessage);
+            input = scanner.nextLine();
+        }
+        String endMessage = "____________________________________________________________\n"
+                + " Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________";
+        System.out.println(endMessage);
     }
 }
