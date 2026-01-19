2837101d6ec93c8a409eedde366651c4d6143dec
Teh Hock Jian <hockjianteh@gmail.com>
2026-01-19 15:15:03 +0800

    Create Task.java

---
+++ src/main/java/Task.java
@@ -0,0 +1,25 @@
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public boolean getStatus() {
+        return this.isDone;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    public void markDone() {
+        this.isDone = true;
+    }
+
+    public void markUnDone() {
+        this.isDone = false;
+    }
+}
