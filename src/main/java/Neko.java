837a25b4d4dd220f6cf5004c4fcd51a5216931e2
Teh Hock Jian <hockjianteh@gmail.com>
2026-01-20 17:59:50 +0800

    Track ToDo, Deadline, Event Tasks with Neko.java

--- src/main/java/Neko.java
+++ src/main/java/Neko.java
@@ -1,6 +1,11 @@
 import java.util.Scanner;
 
 public class Neko {
+    public enum InputType {
+        DEADLINE, EVENT, TODO, LIST,
+        MARK, UNMARK, DEFAULT
+    }
+
     public static void main(String[] args) {
         // Starting message
         printGreetingMessage();
@@ -15,34 +20,120 @@ public class Neko {
 
         // Scans input until input is "bye"
         while (!input.equals("bye")) {
-            String[] split = input.split(" ");
-
-            if (split.length == 2) {
-                // Assume second element is Integer first on Level-3
-                int index = Integer.parseInt(split[1]);
-                System.out.println("____________________________________________________________");
-                if (split[0].equals("mark")) {
-                    dataArray[index - 1].markDone();
-                    System.out.println(" Nice! I've marked this task as done:");
-                } else if (split[0].equals("unmark")) {
-                    dataArray[index - 1].markUnDone();
-                    System.out.println(" OK, I've marked this task as not done yet:");
-                }
-                System.out.println(dataArray[index - 1]);
-                System.out.println("____________________________________________________________");
-            } else if (input.equals("list")) {
-                // List out data
-                String listOfData = generateListOfTasks(dataArray, currentIndex);
-                printListMessage(listOfData);
-            } else {
-                // Echo message
-                printEchoMessage(input);
-
-                // Add input into dataArray
-                Task task = new Task(input);
-                dataArray[currentIndex] = task;
-                currentIndex++;
+            String[] split = input.split(" ", 2);
+            String inputMessage = split[0].trim().toUpperCase();
+            InputType inputType;
+            try {
+                inputType = InputType.valueOf(inputMessage);
+            } catch (IllegalArgumentException e) {
+                inputType = InputType.DEFAULT;
             }
+
+            switch (inputType) {
+                case DEADLINE: {
+                    String[] content = split[1].split("/by");
+                    String description = content[0].trim();
+                    String by = content[1].trim();
+
+                    // Add deadline
+                    Task deadline = new Deadline(description, by);
+                    dataArray[currentIndex] = deadline;
+                    currentIndex++;
+
+                    // Echo Message
+                    printEchoMessage(deadline.toString(), currentIndex);
+                    break;
+                }
+                case EVENT: {
+                    String[] content = split[1].split("/from");
+                    String description = content[0].trim();
+                    String[] date = content[1].split("/to");
+                    String from = date[0].trim();
+                    String by = date[1].trim();
+
+                    // Add deadline
+                    Task event = new Event(description, from, by);
+                    dataArray[currentIndex] = event;
+                    currentIndex++;
+
+                    // Echo Message
+                    printEchoMessage(event.toString(), currentIndex);
+                    break;
+                }
+                case TODO: {
+                    String description = split[1].trim();
+
+                    // Add deadline
+                    Task todo = new ToDo(description);
+                    dataArray[currentIndex] = todo;
+                    currentIndex++;
+
+                    // Echo Message
+                    printEchoMessage(todo.toString(), currentIndex);
+                    break;
+                }
+                case LIST: {
+                    // List out data
+                    String listOfData = generateListOfTasks(dataArray, currentIndex);
+                    printListMessage(listOfData);
+                    break;
+                }
+                case MARK: {
+                    System.out.println("____________________________________________________________");
+                    int markIndex = Integer.parseInt(split[1]);
+                    dataArray[markIndex - 1].markDone();
+                    System.out.println(" Nice! I've marked this task as done:");
+                    System.out.println(dataArray[markIndex - 1]);
+                    System.out.println("____________________________________________________________");
+                    break;
+                }
+                case UNMARK: {
+                    System.out.println("____________________________________________________________");
+                    int unMarkIndex = Integer.parseInt(split[1]);
+                    dataArray[unMarkIndex - 1].markUnDone();
+                    System.out.println(" OK, I've marked this task as not done yet:");
+                    System.out.println(dataArray[unMarkIndex - 1]);
+                    System.out.println("____________________________________________________________");
+                    break;
+                }
+                default: {
+                    // Echo Message
+                    printEchoMessage(input, currentIndex + 1);
+
+                    // Add input into dataArray
+                    Task task = new Task(input);
+                    dataArray[currentIndex] = task;
+                    currentIndex++;
+                    break;
+                }
+            }
+
+//            if (split.length >= 2) {
+//                // Assume second element is Integer first on Level-3
+//                int index = Integer.parseInt(split[1]);
+//                System.out.println("____________________________________________________________");
+//                if (split[0].equals("mark")) {
+//                    dataArray[index - 1].markDone();
+//                    System.out.println(" Nice! I've marked this task as done:");
+//                } else if (split[0].equals("unmark")) {
+//                    dataArray[index - 1].markUnDone();
+//                    System.out.println(" OK, I've marked this task as not done yet:");
+//                }
+//                System.out.println(dataArray[index - 1]);
+//                System.out.println("____________________________________________________________");
+//            } else if (input.equals("list")) {
+//                // List out data
+//                String listOfData = generateListOfTasks(dataArray, currentIndex);
+//                printListMessage(listOfData);
+//            } else {
+//                // Echo message
+//                printEchoMessage(input);
+//
+//                // Add input into dataArray
+//                Task task = new Task(input);
+//                dataArray[currentIndex] = task;
+//                currentIndex++;
+//            }
 
             // Take next input
             input = scanner.nextLine();
@@ -97,11 +188,13 @@ public class Neko {
         System.out.println(endMessage);
     }
 
-    public static void printEchoMessage(String input) {
+    public static void printEchoMessage(String input, int currentIndex) {
         String echoMessage = "____________________________________________________________\n"
-                + " Neko added: "
+                + " Neko added this task: \n"
                 + input
-                + "\n____________________________________________________________";
+                + "\nNow you have " + currentIndex +
+                " tasks in the list.\n____________________________________________________________";
         System.out.println(echoMessage);
     }
+
 }
