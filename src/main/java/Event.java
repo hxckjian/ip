b8aab981c202024fde604f42fb0691c3b22a1abc
Teh Hock Jian <hockjianteh@gmail.com>
2026-01-28 19:57:31 +0800

    Add formatIntoData method into Deadline.java, Event.java, ToDo.java and Task.java

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,15 +1,26 @@
 public class Event extends Task {
-    private String start;
+    private String from;
     private String by;
 
     public Event(String description, String from, String to) {
         super(description);
-        this.start = from;
+        this.from = from;
+        this.by = to;
+    }
+
+    public Event(String description, String from, String to, boolean isDone) {
+        super(description, isDone);
+        this.from = from;
         this.by = to;
     }
 
     @Override
     public String toString() {
-        return "[E]" + super.toString() + " (from: " + start + " to: " + by + ")";
+        return "[E]" + super.toString() + " (from: " + from + " to: " + by + ")";
+    }
+
+    @Override
+    public String formatIntoData() {
+        return "E | " + super.formatIntoData() + " | " + this.from + " | " + this.by;
     }
 }
