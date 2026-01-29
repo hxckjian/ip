7e4099e3ef2cfe26e943e21d109a3b452b78bc80
Teh Hock Jian <hockjianteh@gmail.com>
2026-01-29 19:17:57 +0800

    Add Storage, UI, TaskList into Neko.java

--- src/main/java/Neko.java
+++ src/main/java/Neko.java
@@ -15,7 +15,43 @@ public class Neko {
         MARK, UNMARK, DELETE, DEFAULT
     }
 
+    private Storage storage;
+    private TaskList tasks;
+    private Ui ui;
+
+    public Neko(String filePath) {
+        ui = new Ui();
+        storage = new Storage(filePath);
+        try {
+            tasks = new TaskList(storage.load());
+        } catch (NekoException e) {
+            ui.showError(e.getMessage());
+            tasks = new TaskList();
+        }
+    }
+
+    public void run() {
+        ui.showGreetingMessage();
+        boolean isExit = false;
+        while (!isExit) {
+            try {
+                String fullCommand = ui.readCommand();
+                ui.showDividerLine(); // show the divider line ("_______")
+                Command c = Parser.parse(fullCommand);
+                c.execute(tasks, ui, storage);
+                isExit = c.isExit();
+            } catch (NekoException e) {
+                ui.showError(e.getMessage());
+            } finally {
+                ui.showDividerLine();
+            }
+        }
+        //...
+    }
+
     public static void main(String[] args) {
+
+        new Neko("data/tasks.txt").run();
         // String array of size 100
         ArrayList<Task> arrList = new ArrayList<>();
 
