8ce72a3e7bd0ae3dbba1679c076304d6f0413ee3
Teh Hock Jian <hockjianteh@gmail.com>
2026-01-29 22:40:00 +0800

    Add functionality to TodoCommand.java

--- src/main/java/command/TodoCommand.java
+++ src/main/java/command/TodoCommand.java
@@ -1,4 +1,21 @@
 package command;
 
-public class TodoCommand {
+import neko.NekoException;
+import neko.Storage;
+import neko.TaskList;
+import neko.Ui;
+import task.Task;
+
+public class TodoCommand extends Command {
+    private final Task todo;
+
+    public TodoCommand(Task deadline) {
+        this.todo = deadline;
+    }
+
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) throws NekoException {
+        tasks.addTask(this.todo);
+        ui.showAddMessage(this.todo, tasks.getSize());
+    }
 }
