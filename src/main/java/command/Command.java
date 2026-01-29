2cd62b0e19acdea5c96a0aec7279f1fb9fac9ffe
Teh Hock Jian <hockjianteh@gmail.com>
2026-01-29 22:45:25 +0800

    Edit command.java with import from neko

--- src/main/java/command/Command.java
+++ src/main/java/command/Command.java
@@ -1,12 +1,12 @@
 package command;
 
-import java.TaskList;
-import java.Ui;
-import java.Storage;
+import neko.NekoException;
+import neko.TaskList;
+import neko.Ui;
+import neko.Storage;
 
 public abstract class Command {
-    public abstract void execute(TaskList tasks, Ui ui, Storage storage)
-            throws NekoException;
+    public abstract void execute(TaskList tasks, Ui ui, Storage storage) throws NekoException;
 
     public boolean isExit() {
         return false;
