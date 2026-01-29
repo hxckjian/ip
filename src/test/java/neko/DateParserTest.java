5242bfa1446d535df94e8191713d8ecb0ffa33ca
Teh Hock Jian <hockjianteh@gmail.com>
2026-01-30 00:12:51 +0800

    Add DateParserTest.java

---
+++ src/test/java/neko/DateParserTest.java
@@ -0,0 +1,24 @@
+package neko;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+public class DateParserTest {
+    @Test
+    public void parseTextIntoDate_validDate_returnsLocalDate() throws NekoException {
+        LocalDate date = DateParser.parseTextIntoDate("2026-01-29");
+        assertEquals(LocalDate.of(2026, 1, 29), date);
+    }
+
+    @Test
+    public void parseTextIntoDate_invalidDate_throwsNekoException() {
+        assertThrows(NekoException.class, () -> {
+            DateParser.parseTextIntoDate("29-01-2026");
+        });
+    }
+}
