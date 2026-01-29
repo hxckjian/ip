6c938d3f4c91e4cba3e83850c22b54b4c23dc744
Teh Hock Jian <hockjianteh@gmail.com>
2026-01-29 22:40:22 +0800

    Add functionality to DeadlineCommand.java

--- src/main/java/command/DeadlineCommand.java
+++ src/main/java/command/DeadlineCommand.java
@@ -1,4 +1,21 @@
 package command;
 
-public class DeadlineCommand {
+import neko.NekoException;
+import neko.Storage;
+import neko.TaskList;
+import neko.Ui;
+import task.Task;
+
+public class DeadlineCommand extends Command {
+    private final Task deadline;
+
+    public DeadlineCommand(Task deadline) {
+        this.deadline = deadline;
+    }
+
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) throws NekoException {
+        tasks.addTask(this.deadline);
+        ui.showAddMessage(this.deadline, tasks.getSize());
+    }
 }
