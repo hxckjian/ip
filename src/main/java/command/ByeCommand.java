f880229857e169b4efb572f627a641bb0fb00fa6
Teh Hock Jian <hockjianteh@gmail.com>
2026-01-29 22:40:13 +0800

    Add functionality to ByeCommand.java

--- src/main/java/command/ByeCommand.java
+++ src/main/java/command/ByeCommand.java
@@ -1,4 +1,19 @@
 package command;
 
-public class ByeCommand {
+import neko.NekoException;
+import neko.Storage;
+import neko.TaskList;
+import neko.Ui;
+
+public class ByeCommand extends Command {
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) throws NekoException {
+        storage.write(tasks);
+        ui.printEndMessage();
+    }
+
+    @Override
+    public boolean isExit() {
+        return true;
+    }
 }
