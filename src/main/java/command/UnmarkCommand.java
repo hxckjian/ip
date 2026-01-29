ca1b54b39e42bc703de29548100db79cd05b4ae2
Teh Hock Jian <hockjianteh@gmail.com>
2026-01-29 22:39:50 +0800

    Add functionality to UnmarkCommand.java

--- src/main/java/command/UnmarkCommand.java
+++ src/main/java/command/UnmarkCommand.java
@@ -1,4 +1,22 @@
 package command;
 
-public class UnmarkCommand {
+import neko.NekoException;
+import neko.Storage;
+import neko.TaskList;
+import neko.Ui;
+import task.Task;
+
+public class UnmarkCommand extends Command {
+    private final int inputIndex;
+
+    public UnmarkCommand(int inputIndex) {
+        this.inputIndex = inputIndex;
+    }
+
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) throws NekoException {
+        tasks.unmarkTask(this.inputIndex);
+        Task unmarkedTask = tasks.getTask(this.inputIndex - 1);
+        ui.showMarkedTask(unmarkedTask);
+    }
 }
