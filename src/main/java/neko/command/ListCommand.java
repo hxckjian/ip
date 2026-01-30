c99128cc8e273200fefb91a818c3ede50d24bc4d
Teh Hock Jian <hockjianteh@gmail.com>
2026-01-30 09:00:26 +0800

    Add JavaDoc comments to ListCommand.java

--- src/main/java/neko/command/ListCommand.java
+++ src/main/java/neko/command/ListCommand.java
@@ -6,6 +6,15 @@ import neko.TaskList;
 import neko.Ui;
 
 public class ListCommand extends Command {
+    /**
+     * Executes the list command.
+     * Displays the current tasks in the task list.
+     *
+     * @param tasks Task list to be modified or accessed.
+     * @param ui User interface used to display output.
+     * @param storage Storage instance for saving data.
+     * @throws NekoException If an error occurs during execution.
+     */
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) throws NekoException {
         ui.printListMessage(tasks.toString());
