ba2e12933840997a87a5615ab6cfd53dcd4ffdd9
Teh Hock Jian <hockjianteh@gmail.com>
2026-01-30 08:56:03 +0800

    Add JavaDoc comments to Command event subclasses

--- src/main/java/neko/command/EventCommand.java
+++ src/main/java/neko/command/EventCommand.java
@@ -9,10 +9,24 @@ import neko.task.Task;
 public class EventCommand extends Command {
     private final Task event;
 
-    public EventCommand(Task deadline) {
-        this.event = deadline;
+    /**
+     * Creates a command to add the specified event task.
+     *
+     * @param event Event task to be added.
+     */
+    public EventCommand(Task event) {
+        this.event = event;
     }
 
+    /**
+     * Executes the event command.
+     * Adds the event task to the task list and displays a confirmation.
+     *
+     * @param tasks Task list to be modified or accessed.
+     * @param ui User interface used to display output.
+     * @param storage Storage instance for saving data.
+     * @throws NekoException If an error occurs during execution.
+     */
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) throws NekoException {
         tasks.addTask(this.event);
--- src/main/java/neko/command/TodoCommand.java
+++ src/main/java/neko/command/TodoCommand.java
@@ -9,10 +9,24 @@ import neko.task.Task;
 public class TodoCommand extends Command {
     private final Task todo;
 
-    public TodoCommand(Task deadline) {
-        this.todo = deadline;
+    /**
+     * Creates a command to add the specified todo task.
+     *
+     * @param todo Todo task to be added.
+     */
+    public TodoCommand(Task todo) {
+        this.todo = todo;
     }
 
+    /**
+     * Executes the todo command.
+     * Adds the todo task to the task list and displays a confirmation.
+     *
+     * @param tasks Task list to be modified or accessed.
+     * @param ui User interface used to display output.
+     * @param storage Storage instance for saving data.
+     * @throws NekoException If an error occurs during command execution.
+     */
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) throws NekoException {
         tasks.addTask(this.todo);
