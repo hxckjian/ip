50bddab16c8f8273ee0fdfe3ed943d3c14ddf3ea
Teh Hock Jian <hockjianteh@gmail.com>
2026-01-30 08:47:08 +0800

    Add JavaDoc comments to Event.java

--- src/main/java/neko/task/Event.java
+++ src/main/java/neko/task/Event.java
@@ -7,18 +7,40 @@ public class Event extends Task {
     private LocalDate from;
     private LocalDate to;
 
+    /**
+     * Creates a new event task with the specified description and date range.
+     * The task is initially marked as not done.
+     *
+     * @param description Description of the event.
+     * @param from Start date of the event.
+     * @param to End date of the event.
+     */
     public Event(String description, LocalDate from, LocalDate to) {
         super(description);
         this.from = from;
         this.to = to;
     }
 
+    /**
+     * Creates a new event task with the specified description,
+     * date range, and completion status.
+     *
+     * @param description Description of the event.
+     * @param from Start date of the event.
+     * @param to End date of the event.
+     * @param isDone Whether the task is completed.
+     */
     public Event(String description, LocalDate from, LocalDate to, boolean isDone) {
         super(description, isDone);
         this.from = from;
         this.to = to;
     }
 
+    /**
+     * Returns a string representation of the event task for display.
+     *
+     * @return Formatted event task string.
+     */
     @Override
     public String toString() {
         String formattedDateFrom = this.from.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
@@ -26,6 +48,11 @@ public class Event extends Task {
         return "[E]" + super.toString() + " (from: " + formattedDateFrom + " to: " + formattedDateTo + ")";
     }
 
+    /**
+     * Formats the event task into a string suitable for file storage.
+     *
+     * @return Serialized event task representation.
+     */
     @Override
     public String formatIntoData() {
         return "E | " + super.formatIntoData() + " | " + this.from + " | " + this.to;
