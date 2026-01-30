2f7c61425c8da0d5f19905ec51be3f7109085f1f
Teh Hock Jian <hockjianteh@gmail.com>
2026-01-30 08:59:29 +0800

    Add JavaDoc comments to Mark and Umark command

--- src/main/java/neko/command/MarkCommand.java
+++ src/main/java/neko/command/MarkCommand.java
@@ -9,10 +9,24 @@ import neko.task.Task;
 public class MarkCommand extends Command {
     private final int inputIndex;
 
+    /**
+     * Creates a command to mark a task at the specified index.
+     *
+     * @param inputIndex One-based index of the task to mark.
+     */
     public MarkCommand(int inputIndex) {
         this.inputIndex = inputIndex;
     }
 
+    /**
+     * Executes the mark command.
+     * Marks the specified task as done and displays a confirmation.
+     *
+     * @param tasks Task list to be modified or accessed.
+     * @param ui User interface used to display output.
+     * @param storage Storage instance for saving data.
+     * @throws NekoException If an error occurs during execution.
+     */
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) throws NekoException {
         tasks.markTask(this.inputIndex);
--- src/main/java/neko/command/UnmarkCommand.java
+++ src/main/java/neko/command/UnmarkCommand.java
@@ -9,10 +9,24 @@ import neko.task.Task;
 public class UnmarkCommand extends Command {
     private final int inputIndex;
 
+    /**
+     * Creates a command to unmark a task at the specified index.
+     *
+     * @param inputIndex One-based index of the task to unmark.
+     */
     public UnmarkCommand(int inputIndex) {
         this.inputIndex = inputIndex;
     }
 
+    /**
+     * Executes the unmark command.
+     * Marks the specified task as not done and displays a confirmation.
+     *
+     * @param tasks Task list to be modified or accessed.
+     * @param ui User interface used to display output.
+     * @param storage Storage instance for saving data.
+     * @throws NekoException If an error occurs during execution.
+     */
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) throws NekoException {
         tasks.unmarkTask(this.inputIndex);
