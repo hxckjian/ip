a042e142a952452b75842f030fd7da40365077ad
Teh Hock Jian <hockjianteh@gmail.com>
2026-01-30 08:49:32 +0800

    Add JavaDoc comments to Command.java

--- src/main/java/neko/command/Command.java
+++ src/main/java/neko/command/Command.java
@@ -6,8 +6,22 @@ import neko.Ui;
 import neko.Storage;
 
 public abstract class Command {
+    /**
+     * Executes the command.
+     *
+     * @param tasks Task list to be modified or accessed.
+     * @param ui User interface used to display output.
+     * @param storage Storage instance for saving data.
+     * @throws NekoException If an error occurs during command execution.
+     */
     public abstract void execute(TaskList tasks, Ui ui, Storage storage) throws NekoException;
 
+    /**
+     * Indicates whether this command should terminate the application.
+     *
+     * @return {@code true} if the command exits the application,
+     *         {@code false} otherwise.
+     */
     public boolean isExit() {
         return false;
     }
