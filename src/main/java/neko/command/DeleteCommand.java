1d1f8ec7df502959dfac7da65e4146681c85539c
Teh Hock Jian <hockjianteh@gmail.com>
2026-01-30 08:57:10 +0800

    Add JavaDoc comments to DeleteCommand.java

--- src/main/java/neko/command/DeleteCommand.java
+++ src/main/java/neko/command/DeleteCommand.java
@@ -9,10 +9,24 @@ import neko.task.Task;
 public class DeleteCommand extends Command {
     public final int inputIndex;
 
+    /**
+     * Creates a command to delete a task at the specified index.
+     *
+     * @param inputIndex One-based index of the task to delete.
+     */
     public DeleteCommand(int inputIndex) {
         this.inputIndex = inputIndex;
     }
 
+    /**
+     * Executes the delete command.
+     * Removes the specified task from the task list and displays a confirmation.
+     *
+     * @param tasks Task list to be modified or accessed.
+     * @param ui User interface used to display output.
+     * @param storage Storage instance for saving data.
+     * @throws NekoException If an error occurs during execution.
+     */
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) throws NekoException {
         Task removedTask = tasks.getTask(inputIndex - 1);
