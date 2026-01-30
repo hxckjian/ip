5131664701c3e40f9132c659e8997fb68f4ff059
Teh Hock Jian <hockjianteh@gmail.com>
2026-01-30 08:51:17 +0800

    Add JavaDoc comments to ByeCommand.java

--- src/main/java/neko/command/ByeCommand.java
+++ src/main/java/neko/command/ByeCommand.java
@@ -6,12 +6,26 @@ import neko.TaskList;
 import neko.Ui;
 
 public class ByeCommand extends Command {
+    /**
+     * Executes the bye command.
+     * Saves tasks to storage and displays the farewell message.
+     *
+     * @param tasks Task list to be modified or accessed.
+     * @param ui User interface used to display output.
+     * @param storage Storage instance for saving data.
+     * @throws NekoException If an error occurs during command execution.
+     */
     @Override
     public void execute(TaskList tasks, Ui ui, Storage storage) throws NekoException {
         storage.write(tasks);
         ui.printEndMessage();
     }
 
+    /**
+     * Indicates that this command exits the application.
+     *
+     * @return {@code true} to signal application termination.
+     */
     @Override
     public boolean isExit() {
         return true;
