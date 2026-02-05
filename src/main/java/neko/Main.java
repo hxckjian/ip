26024535b2e25523be33bae8fd2ea3a6e9d5060a
Teh Hock Jian <hockjianteh@gmail.com>
2026-02-05 16:15:32 +0800

    Add Main.java

---
+++ src/main/java/neko/Main.java
@@ -0,0 +1,36 @@
+package neko;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+/**
+ * A GUI for Duke using FXML.
+ */
+public class Main extends Application {
+
+    // Creates a new Neko instance and save data into file path.
+    private Neko neko = new Neko("data/neko.txt");
+
+    @Override
+    public void start(Stage stage) {
+        try {
+//            System.out.println(Main.
+//                    class.getResource("/view/MainWindow.fxml"));
+            FXMLLoader fxmlLoader = new FXMLLoader(Main.
+                    class.getResource("/view/MainWindow.fxml"));
+            AnchorPane ap = fxmlLoader.load();
+            Scene scene = new Scene(ap);
+            stage.setScene(scene);
+            fxmlLoader.<MainWindow>getController().setNeko(neko);  // inject the Neko instance
+            stage.show();
+
+            fxmlLoader.<MainWindow>getController().showGreetingBox();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
