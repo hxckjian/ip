3cc8dc7847f1dd0f9ec22e8f9908b09d9d95007c
Teh Hock Jian <hockjianteh@gmail.com>
2026-02-12 17:32:44 +0800

    Modify method name

--- src/main/java/neko/command/ByeCommand.java
+++ src/main/java/neko/command/ByeCommand.java
@@ -21,7 +21,7 @@ public class ByeCommand extends Command {
     @Override
     public String execute(TaskList tasks, Ui ui, Storage storage) throws NekoException {
         storage.write(tasks);
-        return ui.printEndMessage();
+        return ui.showEndMessage();
     }
 
     /**
